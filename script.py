43948d3ae36bb52dd57faf633ae586c70ce99dea
get <45425365+Minty-Meeo@users.noreply.github.com>
2024-12-20 10:40:10 -0600

    Translate GameFlow children

---
+++ script.py
@@ -0,0 +1,33 @@
+import csv
+import lief
+import struct
+
+BASE_ADDR = 0x10000000
+I18N_ADDR = 0x004ea000
+
+def main():
+    pe = lief.PE.parse("./plugins/plugPiki.dll")
+    csvfile = csv.reader(open("./JPN to ENG.csv"))
+    
+    rdata = pe.get_section(".rdata")
+    i18n_blob = bytearray()
+    cursor = BASE_ADDR + I18N_ADDR
+
+    for jpn, eng in csvfile:
+        string_location = BASE_ADDR + rdata.virtual_address + rdata.search(jpn.encode("shift-jis"))
+        eng_bytes = eng.encode()
+        for address in pe.xref(string_location):
+            pe.patch_address(BASE_ADDR + address, tuple(struct.pack("<I", cursor)))
+        i18n_blob += eng_bytes + b'\0'
+        cursor += len(eng_bytes) + 1
+
+    i18n = lief.PE.Section(".i18n")
+    i18n.content = i18n_blob
+    i18n.virtual_address = I18N_ADDR
+
+    pe.add_section(i18n, lief.PE.SECTION_TYPES.DATA)
+    pe.write("./files/plugins/plugPiki.dll")
+#
+
+if __name__ == "__main__":
+    main()
