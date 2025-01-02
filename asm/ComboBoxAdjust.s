3a600207b3c22efd36c371b4ebae71faa11e37db
get <45425365+Minty-Meeo@users.noreply.github.com>
2025-01-02 12:52:05 -0600

    ComboBoxAdjust: Adjust extra padding

--- asm/ComboBoxAdjust.s
+++ asm/ComboBoxAdjust.s
@@ -1,6 +1,6 @@
 .intel_syntax noprefix
 mov edx, dword ptr ss:[ebp-0x04]  # EDX = CoreNode ptr from stack
 mov eax, dword ptr ss:[ebp-0x114] # EAX = psizl.cx from stack
-add eax, 20                       # Add +10 padding to left and right
+add eax, 16                       # Add +16 to width.  This seems to line the message up with the +20 padding for slider widgets.
 mov dword ptr ds:[edx+0x4C], eax  # combo_display_x = EAX
 ret
